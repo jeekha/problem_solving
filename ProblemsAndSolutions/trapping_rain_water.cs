c42434d0af2146e2586c6e81abbe339ffb248152
jeetendrakhadka1 <jkhadkase@gmail.com>
2021-06-18 18:08:16 -0500

    optimized solution

--- ProblemsAndSolutions/trapping_rain_water.cs
+++ ProblemsAndSolutions/trapping_rain_water.cs
@@ -13,31 +13,27 @@ Time: O(n*n)
 
 private void TrappingRainWater(int[] arr, int n)
 {
+    var left_max = new int[n];
+    var right_max = new int[n];
+
+    // fill left max array 
+    left_max[0] = arr[0];
+    for (int a = 1; a < n; a++)
+    {
+        left_max[a] = Math.Max(arr[a], left_max[a - 1]);
+    }
+    // fill right max array
+    right_max[n - 1] = arr[n - 1];
+    for (int b = n - 2; b >= 0; b--)
+    {
+        right_max[b] = Math.Max(arr[b], right_max[b + 1]);
+    }
+
     int sum = 0;
     for (int x = 1; x < n - 1; x++)
     {
-        int leftMax = Find_Left_Max(arr, x);
-        int rightMax = Find_Right_Max(arr, n, x);
-        sum = sum + Math.Min(leftMax, rightMax) - arr[x];
+        sum = sum + Math.Min(left_max[x], right_max[x]) - arr[x];
     }
     Console.WriteLine("Total water trapped is " + sum);
 }
 
-private int Find_Left_Max(int[] arr, int x)
-{
-    int max = 0;
-    for (int a = 0; a <= x; a++)
-    {
-        max = Math.Max(max, arr[a]);
-    }
-    return max;
-}
-private int Find_Right_Max(int[] arr, int n, int x)
-{
-    int max = 0;
-    for (int b = n - 1; b >= x; b--)
-    {
-        max = Math.Max(max, arr[b]);
-    }
-    return max;
-}
